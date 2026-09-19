1cb1b86c8a1009eff0f3865e5b594bc5402b9c46
wadeneff <wadenef.ff@gmail.com>
2026-09-20 00:52:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import telebot
+import requests
+import json
+from config import api_key
+from telebot import types
+
+bot = telebot.TeleBot(api_key)
+api = 'effd69c38e8373f0fb540676eff86681'
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, f'Рад вас видеть, {message.from_user.first_name}, напишите свой город.')
+
+@bot.message_handler(content_types=['text'])
+def getWeather(message):
+    cityForApi = message.text.strip().lower()
+    city = message.text.strip()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={cityForApi}&appid={api}&units=metric')
+    data = json.loads(res.text)
+
+    if data['cod'] == '404':
+        bot.send_message(message.chat.id, 'Город не найден, попробуйте ещё раз.')
+        return
+    else:
+        temp = data['main']['temp']
+        tempFeelsLike = data['main']['feels_like']
+        clouds = data['weather'][0]['description']
+
+        if clouds == 'broken clouds':
+            cloudsForUser = '🌥 Облачно'
+        elif clouds == 'few clouds':
+            cloudsForUser = '⛅️ Переменная облачность'
+        elif clouds == 'clear sky':
+            cloudsForUser = '☀️ Ясно'
+        elif clouds == 'overcast clouds':
+            cloudsForUser = '☁️ Сплошная облачность'
+
+        bot.send_message(message.chat.id, f'Температура в {city} сейчас {round(temp)}℃, ощущается как {round(tempFeelsLike)}℃\n{cloudsForUser}')
+
+
+bot.polling(non_stop=True)
